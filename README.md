6618d544cc902e1dfe3c5e6046dc1acf6e62a324
mnagaso <mnsaru22@gmail.com>
2019-03-23 16:18:06 +0100

    automatic make sig list

--- README.md
+++ README.md
@@ -6,9 +6,8 @@ A set of scripts and samples to combine seismo files into single binary file in 
 
 ## Description of included files:  
 * combine_signal_files_into_a_single_one.f90: fortran code which do the combine process.  
-* test_signal_files.txt: a list of signal pathes that will be combined.
-* test_script_convert_signals_to_one_single_binalize.py: sample script for the process.  
-* test_script_open_binarized_file.py: sample script for opening the combined binary file.  
+* test_script_convert_signals_to_one_single_binalize.py: python script for usage of the combine code.  
+* test_script_open_binarized_file.py: sample python script for opening the combined binary file.  
 * compile_on_occigen.sh: useful script when one comile the fortran code on OCCIGEN/CINES. 
 * test_signals: folder where the sample signals are put.  
   
@@ -17,6 +16,5 @@ A set of scripts and samples to combine seismo files into single binary file in 
 1. compile combine_signal_files_into_a_single_one.f90 with compile flags for HDF5  
 `h5pfc (or h5fc) combine_signal_files_into_a_single_one.f90 -o combine`  
 (just do `./compile_on_occigen.sh` if you use it on OCCIGEN)  
-2. prepare list_signal_files.txt which is a list of pathes for seismo signal files.  
-3. `python test_script_convert_signals_to_single_binary.py` then the result will be saved as test_out.h5  
+2. `python test_script_convert_signals_to_single_binary.py -d directory/path/*.semp -o outname.h5` then the result will be saved as outname.h5  
    
--- test_script_convert_signals_to_single_binary.py
+++ test_script_convert_signals_to_single_binary.py
@@ -1,20 +1,64 @@
-import subprocess
+import subprocess, glob
+import re
+from optparse import OptionParser
+
+
+# some functions for sorting signal files take from -> https://nedbatchelder.com/blog/200712/human_sorting.html
+def tryint(s):
+    try:
+        return int(s)
+    except ValueError:
+        return s
+ 
+
+def alphanum_key(s):
+    """ Turn a string into a list of string and number chunks.
+        "z23a" -> ["z", 23, "a"]
+    """
+    return [ tryint(c) for c in re.split('([0-9]+)', s) ]
+
+
+def sort_nicely(l):
+    """ Sort the given list in the way that humans expect.
+    """
+    l.sort(key=alphanum_key)
+
 
 def main():
     """
     """
+    
+    parser = OptionParser()
+    parser.add_option("-d", "--dir", dest="dpath", help="directory path where *semp files are placed", metavar="DIR_PATH", default="OUTPUT_FILES")
+    parser.add_option("-o", "--outfile", dest="fname", help="output filename", metavar="OUTFILE",default="out.h5")
 
-    file_of_signal_list = "list_signal_files.txt" # path to a file which is a list of pathes to .semp files
-    output_file_name    = "test_out.h5"           # name of output binary file
-    total_timesteps     = 5000                    # total number of timesteps in your calculation
-    total_num_sigs      = 5                       # total number of signals (.semp files) to be combined and binarized
+    options, args = parser.parse_args()
+    file_directory_path    = options.dpath
+    output_file_name        = options.fname
+
+    # make a list of .semp files
+    sig_files = glob.glob(file_directory_path+"/*semp")
+    
+    # reorder
+    sort_nicely(sig_files)
+    #print(sig_files)
+
+    # read the last timestep number
+    last_timestep = sum(1 for line in open(sig_files[0]))
+    #print(last_timestep)
 
     # time window for limited extraction of timesteps
-    time_window = [0,total_timesteps-1]  
+    time_window = [0, last_timestep-1]  
+
+    # writeout a file list as an text file
+    fname = "signal_file_list.txt"
+    with open(fname, "w") as f:
+        for i, path in enumerate(sig_files):
+            f.write("./"+path+"\n")
 
     # binarize_signals
-    cmd = './combine ' + file_of_signal_list + ' ' + output_file_name + ' ' + str(total_num_sigs) + \
-                   ' ' + str(time_window[0]) + ' ' +  str(time_window[1]) + ' ' + str(total_timesteps)
+    cmd = './combine ' + fname + ' ' + output_file_name + ' ' + str(len(sig_files)) + \
+                   ' ' + str(time_window[0]) + ' ' +  str(time_window[1]) + ' ' + str(last_timestep)
     subprocess.call(cmd, shell=True)
 
 
